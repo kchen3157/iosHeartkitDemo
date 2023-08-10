367e749a33ac2bd85d857d0f752b8531c589d166
Kyle Chen <kyle.chen111@outlook.com>
2023-08-09 19:25:54 -0700

    Initial Commit

---
+++ heartkitDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  heartkitDemo
+//
+//  Created by Kyle Chen on 2023-08-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ heartkitDemo/heartkitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  heartkitDemoApp.swift
+//  heartkitDemo
+//
+//  Created by Kyle Chen on 2023-08-09.
+//
+
+import SwiftUI
+
+@main
+struct heartkitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
