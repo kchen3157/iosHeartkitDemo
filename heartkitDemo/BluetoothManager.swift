37be09b98bf424f60b39eb26456974e400733daf
Kyle Chen <kyle.chen111@outlook.com>
2023-08-11 11:59:46 -0700

    add working chart, GET logging, etc.

--- heartkitDemo/BluetoothManager.swift
+++ heartkitDemo/BluetoothManager.swift
@@ -17,9 +17,7 @@ let HR_SERVICE_CBUUID = CBUUID(string: "0x180D")
 let HK_HEART_RATE_LABELS: Array<String> = Array(arrayLiteral: "NORMAL", "TACHYCARDIA", "BRADYCARDIA")
 
 
-var sampleData: Array<Float> = Array()
 var startDataCollection = false
-
 
 struct Result {
     var heartRate = UInt32()
@@ -31,12 +29,20 @@ struct Result {
     var arrhythmia = Bool()
 }
 
-var results = Result()
 
 class BluetoothManager: NSObject, ObservableObject, CBCentralManagerDelegate {
     
+
+    
     var centralManager: CBCentralManager!
     var ECGSensorPeripheral: CBPeripheral!
+    @Published var CBCentralManagerState = String("unknown")
+    @Published var sampleDataLog = String()
+    @Published var resultLog = String()
+    @Published var sampleData: Array<Float> = Array()
+    @Published var results = Result()
+
+
     
     
     override init() {
@@ -46,34 +52,48 @@ class BluetoothManager: NSObject, ObservableObject, CBCentralManagerDelegate {
     
     
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        switch central.state {
-        case .unknown:
-            print("central.state is .unknown")
-        case .resetting:
-            print("central.state is .resetting")
-        case .unsupported:
-            print("central.state is .unsupported")
-        case .unauthorized:
-            print("central.state is .unauthorized")
-        case .poweredOff:
-            print("central.state is .poweredOff")
-        case .poweredOn:
-            print("central.state is .poweredOn")
-            startScanning()
-        @unknown default:
-            print("central.state is unknown")
+        DispatchQueue.main.async {
+            switch central.state {
+            case .unknown:
+                self.CBCentralManagerState = "unknown"
+                print("central.state is .unknown")
+            case .resetting:
+                self.CBCentralManagerState = "resetting"
+                print("central.state is .resetting")
+            case .unsupported:
+                self.CBCentralManagerState = "unsupported"
+                print("central.state is .unsupported")
+            case .unauthorized:
+                self.CBCentralManagerState = "unauthorized"
+                print("central.state is .unauthorized")
+            case .poweredOff:
+                self.CBCentralManagerState = "poweredOff"
+                print("central.state is .poweredOff")
+            case .poweredOn:
+                self.CBCentralManagerState = "poweredOn"
+                print("central.state is .poweredOn")
+            @unknown default:
+                self.CBCentralManagerState = "unknown"
+                print("central.state is unknown")
+            }
         }
     }
     
     
     func startScanning() {
-        print("Scanning Start")
         centralManager.scanForPeripherals(withServices: [ECG_SERVICE_CBUUID, HR_SERVICE_CBUUID])
+        print("Scanning Started")
+        DispatchQueue.main.async {
+            self.CBCentralManagerState = "Scanning Started"
+        }
     }
     
     func stopScanning() {
-        print("Scanning Stop")
         centralManager.stopScan()
+        print("Scanning Stopped")
+        DispatchQueue.main.async {
+            self.CBCentralManagerState = "Scanning Stopped"
+        }
     }
     
 }
@@ -88,6 +108,9 @@ extension BluetoothManager: CBPeripheralDelegate {
     }
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        DispatchQueue.main.async {
+            self.CBCentralManagerState = "Connected to \(self.ECGSensorPeripheral.name ?? "nameUnknown")"
+        }
         print("Connected to \(ECGSensorPeripheral.name ?? "nameUnknown")")
         ECGSensorPeripheral.discoverServices([ECG_SERVICE_CBUUID])
     }
@@ -119,21 +142,24 @@ extension BluetoothManager: CBPeripheralDelegate {
     }
     
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        switch characteristic.uuid {
-        case ECG_SAMPLE_DATA_CHARACTERISTIC_CBUUID:
-            let ECGSample = ParseECGSample(from: characteristic)
-            print("Got ECGSample: \(ECGSample)")
-        case ECG_SAMPLE_MASK_CHARACTERISTIC_CBUUID:
-            print("Notified of ECGSampleMask")
-        case ECG_RESULT_CHARACTERISTIC_CBUUID:
-            let ECGResult = ParseECGResult(from: characteristic)
-            print("""
-            Got ECGResult:
-            \(ECGResult)
-            """)
-        default:
-            print("Unhandled Characteristic UUID: \(characteristic.uuid)")
+        DispatchQueue.main.async {
+            switch characteristic.uuid {
+            case ECG_SAMPLE_DATA_CHARACTERISTIC_CBUUID:
+                self.sampleDataLog = self.ParseECGSample(from: characteristic)
+                print("Got ECGSample: \(self.sampleDataLog)")
+            case ECG_SAMPLE_MASK_CHARACTERISTIC_CBUUID:
+                print("Notified of ECGSampleMask")
+            case ECG_RESULT_CHARACTERISTIC_CBUUID:
+                self.resultLog = self.ParseECGResult(from: characteristic)
+                print("""
+                Got ECGResult:
+                \(self.resultLog)
+                """)
+            default:
+                print("Unhandled Characteristic UUID: \(characteristic.uuid)")
+            }
         }
+        
     }
     
     private func ParseECGSample(from characteristic: CBCharacteristic) -> String {
@@ -142,6 +168,7 @@ extension BluetoothManager: CBPeripheralDelegate {
         if (index == 0) {
             startDataCollection = true
             sampleData.removeAll(keepingCapacity: false)
+            resultLog = ""
         }
         
         if (startDataCollection) {
--- heartkitDemo/ContentView.swift
+++ heartkitDemo/ContentView.swift
@@ -7,13 +7,32 @@
 
 import SwiftUI
 import CoreBluetooth
+import Charts
 
 struct ContentView: View {
+    
+    
     @ObservedObject var bluetoothManager = BluetoothManager()
-
+    
     var body: some View {
         VStack {
-            Text("Bluetooth Status: \(bluetoothManager.centralManager.state.rawValue)")
+            Chart(0..<bluetoothManager.sampleData.count, id: \.self) { nr in
+                LineMark(
+                    x: .value("X values", nr),
+                    y: .value("Y values", bluetoothManager.sampleData[nr])
+                )
+                .lineStyle(.init(lineWidth: 1))
+            }
+            .frame(width: 400, height: 200)
+            .chartYScale(domain: -6...6)
+            .chartXScale(domain: 0...2500)
+            
+            Text("Bluetooth Status: \(bluetoothManager.CBCentralManagerState)")
+            Text("ECGSample Get: \(bluetoothManager.sampleDataLog)")
+            Text("""
+                 ECGResult Get:
+                 \(bluetoothManager.resultLog)
+                 """)
             Button("Start Scanning") {
                 bluetoothManager.startScanning()
             }
